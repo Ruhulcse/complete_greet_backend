06b92574de338a79bd49dea8a98ac7fa44255e59
ruhulcse <ruhul.cse7862@gmail.com>
2023-02-21 11:42:40 +0600

    port dynamic

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const { route } = require("./routes");
 const app = express();
+
+require('dotenv').config()
+
 app.use(cors());
 app.use(express.json());
-app.use(route)
+// app.use(route)
 
 app.get("/", function (req, res) {
   res.send("Backend is running successfully....");
 });
 
-const PORT = process.env.PORT||3000;
+const PORT =5000;
 app.listen(
   PORT,
   console.log(
