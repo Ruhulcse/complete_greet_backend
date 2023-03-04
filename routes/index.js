0e1831bf0144cf89e8d7653b53e95cc7149e10a6
BelayetH <belayet@evaly.com.bd>
2023-03-04 12:47:05 +0600

    add visitor api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const users = require('../routes/userRoute');
 const notes = require('../routes/noteRoute');
 const bugs = require('../routes/bugRoute');
+const visitors = require('../routes/visitorRoute');
 
 module.exports = [
     users,
     notes,
     bugs,
+    visitors,
 ];
